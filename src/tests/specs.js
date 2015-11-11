2c88a0011424513b8364841243febb14ad080fff
ellery coleman <ellery coleman>
2015-11-11 10:17:44 -0500

    Initial working version of project framework.

---
+++ src/tests/specs.js
@@ -0,0 +1,34 @@
+describe('ControllerOne', function(){
+  it("should be true", function(){
+    expect(true).toBe(true);
+  });
+});
+describe('ControllerTwo', function(){
+
+});
+xdescribe('UserFact', function(){
+
+  describe('remove', function(){
+    it("should remove one item from users", function(){
+
+    });
+  });
+});
+
+xdescribe('UserSvc', function(){
+
+  describe('remove', function(){
+    it("should remove one item from users", function(){
+
+    });
+  });
+});
+
+xdescribe('UsersHttp', function(){
+
+  describe('getUsers', function(){
+    it("should return some users", function(){
+
+    });
+  });
+});
