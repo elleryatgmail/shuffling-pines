7a418ac4bf32b26d6708642e5e4df07f563a29c1
ellery coleman <ellery coleman>
2015-11-11 21:27:38 -0500

    - implements a controller and attaches it to the submit button.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 var app = angular.module('shuffling', []);
 
-app.controller('FormController', [function(){
+app.controller('LogEverything', ['$scope',function($scope){
+  var le= this;
 
-
+  le.writeConsole= function(obj){
+     console.log(obj);
+  };
 }]);
 
 app.controller('TabController', [function(){
+
 
 }]);
