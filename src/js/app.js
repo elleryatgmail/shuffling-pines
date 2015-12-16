b87ffc8527576707de33d8cecad7bc9c20077218
ellery coleman <ellery coleman>
2015-12-15 21:52:14 -0600

    binds form variables to controller.

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,31 @@
 var app = angular.module('shuffling', []);
 
-app.controller('LogEverything', ['$scope',function($scope){
+app.controller('LogEverything', ['elService',function(elService){
   var le= this;
 
-  le.writeConsole= function(obj){
-     console.log(obj);
+
+  le.writeConsole= function(str){
+     console.log("str:" + str);
+	 console.log("guest name: " + le.guestname);
+	 console.log("transition date: " + le.transdate);
+	 console.log("pickup: " + le.pickup);
+	 console.log("dropoff: " + le.dropoff);
+	 console.log("location: " + le.location);
+	 elService.talk();
   };
+
+
+
 }]);
+
+
+app.service('elService', [function(){
+    this.talk= function(){
+	   console.log("elService() at your service...");
+	};	   
+
+}]);
+
 
 app.controller('TabController', [function(){
 
