2be4c8c012b4c96779c437039b5e0ea299d1b0cb
ellery coleman <ellery coleman>
2015-12-16 13:04:23 -0600

    Changed controller name from collectData for formController.

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,35 @@
+
+
+// Application
 var app = angular.module('shuffling', []);
 
-app.controller('collectData', ['elService','changeTab',function(elService,changeTab){
-  var cd= this;
 
-  cd.myStorage= localStorage;
-  cd.myStorage.setItem('myname','ellery');
 
-  cd.writeConsole= function(str){
+// Controller 1 of 2
+app.controller('formController', ['elService','changeTab',function(elService,changeTab){
+  var fc= this;
+
+  fc.myStorage= localStorage;
+  fc.myStorage.setItem('myname','ellery');
+
+  fc.writeConsole= function(str){
      console.log("str:" + str);
-	 console.log("guest name: " + cd.guestname);
-	 console.log("transition date: " + cd.transdate);
-	 console.log("pickup: " + cd.pickup);
-	 console.log("dropoff: " + cd.dropoff);
-	 console.log("location: " + cd.location);
+	 console.log("guest name: " + fc.guestname);
+	 console.log("transition date: " + fc.transdate);
+	 console.log("pickup: " + fc.pickup);
+	 console.log("dropoff: " + fc.dropoff);
+	 console.log("location: " + fc.location);
 	 elService.talk();
 	 changeTab.execute();
-	 console.log("myStorage: " + cd.myStorage);
+	 console.log("myStorage: " + fc.myStorage);
   };
-
-
 
 }]);
 
 
+
+
+// Service 1 of 2
 app.service('elService', [function(){
     this.talk= function(){
 	   console.log("elService() at your service...");
@@ -31,6 +38,9 @@ app.service('elService', [function(){
 }]);
 
 
+
+
+// Service 2 of 2
 app.service('changeTab', [function(){
 	this.execute= function(){
        var target = $(".nav-tabs li.active");
@@ -44,7 +54,13 @@ app.service('changeTab', [function(){
 
 
 
-app.controller('TabController', [function(){
+
+
+// Controller 2 of 2
+app.controller('guestController', [function(){
+
+
+
 
 
 }]);
